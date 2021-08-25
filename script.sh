57b16c7636eac5c9e9f06798ea39ffacde947731
YaelHeger <76470383+YaelHeger@users.noreply.github.com>
2021-08-25 13:12:58 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu"* ]];
+
         then
                 if [ -f "/etc/debian_version" ];
+                
                         then
                         sudo apt update && sudo apt -y upgrade
                         sudo apt install -y gnupg2 curl wget
@@ -12,6 +14,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
                         telegraf -config telegraf.conf
 
                 else
+                
                         wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.8.x86_64.rpm
                         sudo yum localinstall influxdb2-2.0.8.x86_64.rpm
                         wget https://dl.influxdata.com/telegraf/releases/telegraf-1.19.3-1.x86_64.rpm
@@ -19,36 +22,15 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
                         telegraf -config telegraf.conf
                 fi
 
-
 elif [[ "$OSTYPE" == "darwin"* ]];
+
         then
         brew update
         brew install telegraf
         telegraf -config telegraf.conf
 
-# elif [[ "$OSTYPE" == "cygwin" ]]; # POSIX compatibility layer and Linux environment emulation for Windows
-#         then
-#         echo "hello"
-# elif [[ "$OSTYPE" == "msys" ]]; # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-#         then
-#         echo "hello"
-# elif [[ "$OSTYPE" == "win32" ]]; 
-#         then
-#         cd Downloads
-#         wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.8-windows-amd64.zip -UseBasicParsing -OutFile influxdb2-2.0.8-windows-amd64.zip
-#         Expand-Archive .\influxdb2-2.0.8-windows-amd64.zip -DestinationPath 'C:\Program Files\InfluxData\influxdb\'
-#         Expand-Archive .\telegraf.zip 'C:\Program Files\'
-#         mkdir 'C:\Program Files\telegraf\conf'
-#         cd 'C:\Program Files\telegraf\conf'
-#         copy ..\telegraf.conf inputs.conf
-#         .\telegraf --config-directory 'C:\Program Files\telegraf\conf' --test
-
-# elif [[ "$OSTYPE" == "freebsd"* ]]; 
-#         then
-#         sudo pkg install telegraf
-#         sysrc telegraf_enable=yes
-#         service telegraf start
 else
+
         echo "Sorry, this demo does not support your operation system"
 
 fi
