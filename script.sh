62e61935f1c6b0657aedb6302443aabf43398f22
YaelHeger <76470383+YaelHeger@users.noreply.github.com>
2021-09-01 12:55:41 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,20 +5,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
                 if [ -f "/etc/debian_version" ];
                 
                         then
-                        sudo apt update && sudo apt -y upgrade
-                        sudo apt install -y gnupg2 curl wget
-                        wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-                        echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-                        sudo apt update
-                        sudo apt -y install telegraf
+                        curl https://github.com/logzio/telegraf_demo/blob/main/telegraf.conf
+                        sudo dpkg -i telegraf_1.19.3-1_amd64.deb
                         telegraf -config telegraf.conf
 
-                else
-                
-                        wget https://dl.influxdata.com/influxdb/releases/influxdb2-2.0.8.x86_64.rpm
-                        sudo yum localinstall influxdb2-2.0.8.x86_64.rpm
-                        wget https://dl.influxdata.com/telegraf/releases/telegraf-1.19.3-1.x86_64.rpm
-                        sudo yum localinstall telegraf-1.19.3-1.x86_64.rpm
+                else              
+                        curl https://github.com/logzio/telegraf_demo/blob/main/telegraf.conf
                         telegraf -config telegraf.conf
                 fi
 
