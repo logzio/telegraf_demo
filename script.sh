8bdd37b4ec0e7022f8fd9493f8fbd30b7303d355
YaelHeger <76470383+YaelHeger@users.noreply.github.com>
2021-09-01 17:11:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,13 +19,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
                 if [ -f "/etc/debian_version" ];
                 
                         then
-                        curl -L https://raw.githubusercontent.com/logzio/telegraf_demo/main/telegraf.conf -o telegraf.conf
+                        
                         curl -L https://dl.influxdata.com/telegraf/releases/telegraf_1.19.3-1_amd64.deb -o telegraf_1.19.3-1_amd64.deb
                         sudo dpkg -i telegraf_1.19.3-1_amd64.deb
                         telegraf -config telegraf.conf
 
                 else              
-                        curl -L https://raw.githubusercontent.com/logzio/telegraf_demo/main/telegraf.conf -o telegraf.conf
                         curl -L https://dl.influxdata.com/telegraf/releases/telegraf-1.19.3-1.x86_64.rpm -o elegraf-1.19.3-1.x86_64.rpm
                         sudo yum localinstall telegraf-1.19.3-1.x86_64.rpm
                         telegraf --config telegraf.conf
@@ -34,7 +33,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
 elif [[ "$OSTYPE" == "darwin"* ]];
 
         then
-        curl -L https://raw.githubusercontent.com/logzio/telegraf_demo/main/telegraf.conf -o telegraf.conf
+        
         telegraf-1.19.3_darwin_amd64.dmg
         telegraf -config telegraf.conf
 
