d3e8d03b5095cab76a14518b5a0a711f57ac1924
YaelHeger <76470383+YaelHeger@users.noreply.github.com>
2021-09-02 18:55:41 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -27,7 +27,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
                 else              
                         curl -L https://dl.influxdata.com/telegraf/releases/telegraf-1.19.3_linux_amd64.tar.gz -o telegraf-1.19.3_linux_amd64.tar.gz
                         tar -xf telegraf-1.19.3_linux_amd64.tar.gz
-                        telegraf-1.19.3 --config telegraf.conf
+                        ~/telegraf-1.19.3/etc/telegraf --config telegraf.conf
                 fi
 
 elif [[ "$OSTYPE" == "darwin"* ]];
