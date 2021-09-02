d55e4305b8b3a2f3778da85432afe8fe35474cfb
YaelHeger <76470383+YaelHeger@users.noreply.github.com>
2021-09-02 19:42:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 if [[ $TYPE == "system" ]];
 
         then
+        
         curl -L https://raw.githubusercontent.com/logzio/telegraf_demo/main/telegraf_system.conf -o telegraf.conf        
         
-
 else
 
         echo "Sorry, we don't have a configuration file for this type"
@@ -27,7 +27,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]];
                 else              
                         curl -L https://dl.influxdata.com/telegraf/releases/telegraf-1.19.3_linux_amd64.tar.gz -o telegraf-1.19.3_linux_amd64.tar.gz
                         tar -xf telegraf-1.19.3_linux_amd64.tar.gz
-                        ~/telegraf-1.19.3/etc/telegraf --config telegraf.conf
+                        ./telegraf --config telegraf.conf
                 fi
 
 elif [[ "$OSTYPE" == "darwin"* ]];
